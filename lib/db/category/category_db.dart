34ba44ba83fffead45d830f8b28ddc067727b21b
Ismail K <ismailkoormath037@gmail.com>
2023-07-05 12:21:48 +0530

    create refreshUI function in category db

--- lib/db/category/category_db.dart
+++ lib/db/category/category_db.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:money_management/models/category/category_model.dart';
+import 'package:money_management/screens/category_screen/income_category_list.dart';
 
 const CATEGORY_DB_NAME = 'category-database';
 
@@ -10,18 +11,38 @@ abstract class CategoryDbFunction {
 }
 
 class CategoryDB implements CategoryDbFunction {
-  ValueNotifier<List<CategoryModel>> incomeCategory = ValueNotifier([]);
-  ValueNotifier<List<CategoryModel>> expenseCategory = ValueNotifier([]);
+  ValueNotifier<List<CategoryModel>> incomeCategoryListListener =
+      ValueNotifier([]);
+  ValueNotifier<List<CategoryModel>> expenseCategoryListListener =
+      ValueNotifier([]);
 
   @override
   Future<void> insertCategory(CategoryModel value) async {
     final _categoryDB = await Hive.openBox<CategoryModel>(CATEGORY_DB_NAME);
     await _categoryDB.add(value);
+    refreshUI();
   }
 
   @override
   Future<List<CategoryModel>> getCatergories() async {
     final _categoryDB = await Hive.openBox<CategoryModel>(CATEGORY_DB_NAME);
     return _categoryDB.values.toList();
+  }
+
+  Future<void> refreshUI() async {
+    final _allCategories = await getCatergories();
+    incomeCategoryListListener.value.clear();
+    expenseCategoryListListener.value.clear();
+    await Future.forEach(
+        _allCategories,
+        (CategoryModel category) => {
+              if (category.type == CategoryType.income)
+                {incomeCategoryListListener.value.add(category)}
+              else
+                {expenseCategoryListListener.value.add(category)}
+            });
+
+    incomeCategoryListListener.notifyListeners();
+    expenseCategoryListListener.notifyListeners();
   }
 }
